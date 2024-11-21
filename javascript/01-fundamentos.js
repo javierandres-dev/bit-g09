f7968e335550b2b9ffbe440b37c06278338ae209
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-11-20 21:31:46 -0500

    javascript

---
+++ javascript/01-fundamentos.js
@@ -0,0 +1,19 @@
+// Esto es un comentario de JavaScript de una sola línea
+
+/*
+Esto es un comentario
+de JavaScript
+en varias líneas
+*/
+
+// en la siguiente línea estoy imprimiento un mensaje en consola
+console.log('hola, a todos!');
+
+/* VARIABLES */
+// Declaración
+var nombre1;
+let nombre1a;
+
+// declaración e inicialización
+var nombre2 = 'pepita';
+let nombre2a = 'pepito';

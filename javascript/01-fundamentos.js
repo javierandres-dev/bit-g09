cb6636a24dbf150db24a030ff0639157e7e74f51
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-11-21 19:00:47 -0500

    javascript

--- javascript/01-fundamentos.js
+++ javascript/01-fundamentos.js
@@ -6,8 +6,8 @@ de JavaScript
 en varias líneas
 */
 
-// en la siguiente línea estoy imprimiento un mensaje en consola
-console.log('hola, a todos!');
+// en la siguiente línea estoy imprimiento un valor en consola
+// console.log('hola, a todos!');
 
 /* VARIABLES */
 // Declaración
@@ -17,3 +17,38 @@ let nombre1a;
 // declaración e inicialización
 var nombre2 = 'pepita';
 let nombre2a = 'pepito';
+
+// variable de tipo constante
+const miConstante = 1;
+// console.log(miConstante);
+
+// automática
+// miVariableAutomatica;
+
+let miVariable = 'hola';
+// console.log(miVariable);
+miVariable = 'hello';
+// console.log(miVariable);
+
+/* TIPOS DE DATO PRIMITIVOS */
+miVariable = 'Hola, otra vez!';
+miVariable = "I'm";
+miVariable = 'Él dijo: "Hola"';
+miVariable = `Ella dijo: ${nombre2}`;
+miVariable = 'Él pensó en: ' + nombre2;
+miVariable = '1';
+miVariable = 'true';
+miVariable = '';
+
+miVariable = 0;
+miVariable = 10;
+miVariable = 9.5;
+miVariable = 0xff;
+miVariable = 123e5;
+miVariable = -10;
+
+miVariable = true;
+miVariable = false;
+
+miVariable = undefined;
+console.log(miVariable);

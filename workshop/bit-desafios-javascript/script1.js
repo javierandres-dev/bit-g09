a51742e849f80a09440e81164fa4f511193377d8
javi <55107141+javierandres-dev@users.noreply.github.com>
2024-12-16 20:43:27 -0500

    workshop

---
+++ workshop/bit-desafios-javascript/script1.js
@@ -0,0 +1,10 @@
+/*
+1. Crear una variable y asignarle su nombre, crear una constante y asignarle su
+edad, luego imprimir en consola: Hola, soy ____ y tengo ____ años.
+*/
+let miNombre = 'Pepita';
+const miEdad = 32;
+
+console.log('Hola, soy ' + miNombre + ' y tengo ' + miEdad + ' años.');
+
+console.log(`Hola, soy ${miNombre} y tengo ${miEdad} años.`);

eb609c89ca8ecdd0ae73c8b894c1a481127919d4
javi <55107141+javierandres-dev@users.noreply.github.com>
2025-01-20 21:51:14 -0500

    javascript

---
+++ javascript/interactivo/js/script.js
@@ -0,0 +1,11 @@
+/* INTERACCIÓN */
+
+/* Entrada | Inputs */
+// const dato = prompt();
+//const nombreUsuario = prompt('Por favor, ingrese su nombre:');
+//console.log(nombreUsuario);
+
+/* Salida | Output */
+// console.log('¡Funciona!');
+// document.write('¡Funciona!');
+// alert('¡Funciona!');

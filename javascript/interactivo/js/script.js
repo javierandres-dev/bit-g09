93946ede9101366ec0574f89df7c902fffd7fbfb
javi <55107141+javierandres-dev@users.noreply.github.com>
2025-01-21 21:22:55 -0500

    javascript

--- javascript/interactivo/js/script.js
+++ javascript/interactivo/js/script.js
@@ -1,11 +1,75 @@
+'use strict';
 /* INTERACCIÓN */
 
 /* Entrada | Inputs */
 // const dato = prompt();
-//const nombreUsuario = prompt('Por favor, ingrese su nombre:');
-//console.log(nombreUsuario);
+// const nombreUsuario = prompt('Por favor, ingrese su nombre:');
+// console.log(nombreUsuario);
 
 /* Salida | Output */
 // console.log('¡Funciona!');
 // document.write('¡Funciona!');
 // alert('¡Funciona!');
+
+/* VARIABLES */
+// automática
+/* nombreUsuario = 'Pepita';
+console.log(nombreUsuario); */
+
+// var
+/* var apellidoUsuario = 'Pérez';
+console.log(apellidoUsuario); */
+
+// let
+/* let edadUsuario = 20;
+console.log(edadUsuario); */
+
+// const
+/* const contrasenia = 123;
+console.log(contrasenia); */
+
+/* BLOQUES y ÁMBITO DE LAS VARIABLES | SCOPE */
+/* const numeroGlobal = 10;
+console.log(numeroGlobal); */
+
+/* var textoGlobal = 'HOLA';
+console.log(textoGlobal); */
+
+/* let variableGlobal = 'hola soy LET GLOBAL';
+console.log(variableGlobal); */
+
+/* {
+  const numeroLocal = 11;
+  console.log(numeroLocal);
+
+  let texto1Local = 'texto 1 con LET LOCAL';
+  console.log(texto1Local);
+
+  console.log(numeroGlobal);
+  console.log(textoGlobal);
+  console.log(variableGlobal);
+
+  var texto2Local = 'texto 2 con VAR LOCAL';
+  console.log(texto2Local);
+}
+
+console.log(texto2Local);
+console.log(texto1Local); */
+
+/* TIPOS DE DATO PRIMITIVOS */
+/* const nombre = prompt('Escriba su nombre:');
+console.log(nombre); */
+
+/* OPERADOR typeof */
+/* const nombre = 'Pepita';
+const edad = 20;
+const autorizado = true;
+const miVariable = undefined;
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof autorizado);
+console.log(typeof miVariable);
+
+const valor = null;
+console.log(typeof valor); */

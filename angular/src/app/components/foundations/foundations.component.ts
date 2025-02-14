975d890c0c504b6ab5d9fa39bea96b9db3b8ffe1
javi <55107141+javierandres-dev@users.noreply.github.com>
2025-02-13 21:45:07 -0500

    angular

--- angular/src/app/components/foundations/foundations.component.ts
+++ angular/src/app/components/foundations/foundations.component.ts
@@ -4,8 +4,41 @@ import { Component } from '@angular/core';
   selector: 'app-foundations',
   imports: [],
   templateUrl: './foundations.component.html',
-  styleUrl: './foundations.component.css'
+  styleUrl: './foundations.component.css',
 })
 export class FoundationsComponent {
+  nombreUsuario: string = 'Pepita';
+  estaLogueado: boolean = false;
+  devs: string[] = [
+    'Alexandra Restrepo',
+    'Brayan Narvaez',
+    'Daniel Cruz',
+    'Isaac Diaz',
+    'Javier Sanabria',
+    'Juan Aguirre',
+    'Manuela Álvarez',
+    'Sergio Novoa',
+    'Valentina Restan',
+  ];
+  ooss = [
+    { id: 'win', name: 'Windows' },
+    { id: 'osx', name: 'MacOS' },
+    { id: 'linux', name: 'Linux' },
+  ];
+  evitarEnvio: boolean = true;
+  urlImagen: string =
+    'https://bitinstitute.co/cms/wp-content/uploads/bit-logo.svg';
+  colorFondo: string = 'cf-rojo';
 
+  manejarClick() {
+    console.log('Hiciste click');
+  }
+
+  manejarMouseover() {
+    this.colorFondo = 'cf-amarillo';
+  }
+
+  manejarMouseleave() {
+    this.colorFondo = 'cf-naranja';
+  }
 }

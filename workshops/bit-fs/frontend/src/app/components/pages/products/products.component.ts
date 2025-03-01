6ba61a23d55bacce53a05ba4a62f36b30a07b77f
javi <55107141+javierandres-dev@users.noreply.github.com>
2025-02-28 19:27:34 -0500

    workshop

--- workshops/bit-fs/frontend/src/app/components/pages/products/products.component.ts
+++ workshops/bit-fs/frontend/src/app/components/pages/products/products.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../../services/products.service';
+import { Product } from '../../../interfaces/products';
 
 @Component({
   selector: 'app-products',
@@ -7,12 +8,19 @@ import { ProductsService } from '../../../services/products.service';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   productsService = inject(ProductsService);
+
+  productos: any = null;
+
+  ngOnInit() {
+    this.obtenerTodosLosProductos();
+  }
 
   obtenerTodosLosProductos() {
     this.productsService.getAllProducts().subscribe((respuesta: any) => {
       console.log('respuesta:', respuesta);
+      this.productos = respuesta.data;
     });
   }
 }
---
+++ workshops/bit-fs/frontend/src/app/interfaces/products.ts
@@ -0,0 +1,5 @@
+export interface Product {
+  titulo: string;
+  precio: number;
+  imagen: string;
+}
